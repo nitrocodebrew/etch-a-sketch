5d7fe081f2c024fb7440b82577744dd2dbb3a4a3
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2026-06-21 23:47:50 -0400

    Implement generateSketchGrid
    generateSketchGrid() logic and initial test

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ const $ = q => document.querySelector(q);
 
 // UI elements
 const sketchGridContainer = $('.sketch-grid-container');
+
+// Dynamically generate a custom sized sketch grid
+const generateSketchGrid = size => {
+    // First, clear any existing content in the container
+    sketchGridContainer.innerHTML = '';
+
+    let i = 0;
+    for(; i < (size * size);) {
+        const gridCell = document.createElement('div');
+        gridCell.classList.add('sketch-grid-cell');
+        gridCell.style.height = `calc(100% / ${size})`;
+        gridCell.style.width = `calc(100% / ${size})`;
+        sketchGridContainer.appendChild(gridCell);
+        i++;
+    }
+}
+
+// Initialize default 16 by 16 grid
+generateSketchGrid(16);
