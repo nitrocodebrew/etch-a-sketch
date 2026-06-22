d34bf8ef2629d7fb1434e389704d1354b49abcd3
John Smicklo <204639360+nitrocodebrew@users.noreply.github.com>
2026-06-22 00:50:12 -0400

    Implement New Sketch Grid button functionality
    Prompt the user for grid cell size, and create a square grid based off of the custom input size

--- js/app.js
+++ js/app.js
@@ -35,3 +35,31 @@ const generateSketchGrid = size => {
 
 // Initialize default 16 by 16 grid
 generateSketchGrid(16);
+
+// Let the user define a custom grid size
+newSketchGridBtn.addEventListener('click', clickEvent => {
+    clickEvent.stopPropagation();
+
+    const gridSizeInput = prompt(
+        "Enter a number between 1 and 100. This will create a square sketch grid to work on with those dimensions.",
+        16
+    );
+
+    // If the user clicks cancel, immediately exit the listener to prevent errors
+    if(null === gridSizeInput) return;
+
+    const gridCellSize = +gridSizeInput.trim();
+
+    // Input is not numeric
+    if(Number.isNaN(gridCellSize)) {
+        alert('Please enter a valid number (1 - 100).');
+        return;
+    }
+
+    if(!gridCellSize) gridCellSize = 16;
+    else if(gridCellSize < 1) gridCellSize = 1;
+    else if(gridCellSize > 100) gridCellSize = 100;
+
+    generateSketchGrid(gridCellSize);
+    
+});
